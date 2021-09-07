e98406efaa79e633e279fc4b7566839a95fc0753
richbruzzese <85512661+richbruzzese@users.noreply.github.com>
2021-09-07 14:24:30 -0400

    Added all routes
    - Routes for course and user creation
    - Validation added for creating a new user

--- routes/courses.js
+++ routes/courses.js
@@ -5,14 +5,46 @@ const { asyncHandler } = require('../middleware/async-handler')
 const { Course } = require('../models')
 const router = express.Router()
 
-router.get('/courses', asyncHandler(async (req, res) =>{
+router.get('/courses', asyncHandler(async ( req, res ) =>{
     const course = await Course.findAll()
     res.json(course)
 }))
 
-router.get('/courses/:id', asyncHandler(async( req, res) => {
+router.get('/courses/:id', asyncHandler(async( req, res ) => {
     const course = await Course.findByPk(req.params.id)
     res.json(course)
+}))
+
+router.post('/courses', asyncHandler(async ( req, res ) => {
+    
+        const course = await Course.create(req.body)
+        res.status(201).location(`/api/courses/${course.id}`).json({message: 'Course Created'})
+     
+}))
+
+router.put('/courses/:id', asyncHandler(async ( req, res ) =>{
+    try{
+        const course = await Course.findByPk(req.params.id)
+        if(course){
+            await course.update(req.body)
+            res.status(204).end()
+        }else{
+            res.status(404).end()
+        }
+    }catch(error){
+        if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+            const errors = error.errors.map(err => err.message);
+            res.status(400).json({ errors });   
+          } else {
+            throw error;
+          }
+        }
+}))
+
+router.delete('/courses/:id', asyncHandler(async ( req, res ) =>{
+    const course = await Course.findByPk(req.params.id)
+    await course.destroy()
+    res.status(204).end()
 }))
 
 module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ const { asyncHandler } = require('../middleware/async-handler')
 const { User } = require('../models')
 const { authenticateUser } = require('../middleware/auth-user')
 const router = express.Router()
+const bcrypt = require('bcrypt')
 
 router.get('/users', asyncHandler(async (req, res) =>{
     const user = await User.findAll()
@@ -13,8 +14,40 @@ router.get('/users', asyncHandler(async (req, res) =>{
 
 router.post('/users', asyncHandler(async (req, res) => {
     try {
-      await User.create(req.body);
-      res.status(201).location('/').json({ "message": "Account successfully created!" });
+      const user = req.body
+      const errors = []
+
+      //Check if First Name is included in request
+      if (!user.firstName){
+        errors.push('First Name is required')
+      }
+      //Check if Last Name included
+      if(!user.lastName){
+        errors.push('Last Name is required')
+      }
+      //Check if Email is provided
+      if(!user.emailAddress){
+        errors.push('Email Address is required')
+      }
+      //Check if Password Provided
+      let password = user.password
+      if(!password){
+        errors.push('Password is required')
+      }else{
+        //If Password Provided, hash the provided password
+        user.password = bcrypt.hashSync(password, 10)
+      }
+      //If any errors present, return those errors to user
+      if( errors.length > 0 ){
+        res.status(400)
+        .json({errors})
+      }else{
+        //If no errors present, create the new user in the DB
+        await User.create(user)
+        res.status(201)
+        .location('/')
+        .json({ "message": "Account successfully created!" });
+      }
     } catch (error) {
       if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
         const errors = error.errors.map(err => err.message);
@@ -22,7 +55,6 @@ router.post('/users', asyncHandler(async (req, res) => {
       } else {
         throw error;
       }
-    }
-  }));
+    }}));
 
 module.exports = router
